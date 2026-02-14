056b0717d63fc92707bf192308d8893c8f31d8e7
MUHAMMAD YUSUF ABDURROHMAN <mylearning1505@gmail.com>
2026-02-14 18:37:27 +0000

    Move Chaquopy plugin to the end

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application")
-    id("com.chaquo.python")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.chaquo.python")
 }
 
 android {
