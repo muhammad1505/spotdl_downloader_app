8bbae17617570ea37e1445c2d4a2f022287bfa97
MUHAMMAD YUSUF ABDURROHMAN <mylearning1505@gmail.com>
2026-02-14 18:27:40 +0000

    Fix plugin order

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     id("com.chaquo.python")
+    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
